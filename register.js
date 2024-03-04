1cd65c29c04cfb37e1c27f7aa8745bda0ecdda6a
illestcy <cyrjmishe@gmail.com>
2024-03-04 09:11:58 +0800

    RegWeb

---
+++ register.js
@@ -0,0 +1,34 @@
+console.log('Haluu');
+
+
+
+
+
+
+
+let btnRegister = document.querySelector
+("#btnRegister")
+let txtUsername = document.querySelector
+("#txtUsername")
+let txtPassword = document.querySelector
+("#txtPassword")
+let cnfrmPassword = document.querySelector
+("#cnfrmPassword")
+let txtAge = document.querySelector
+("#txtAge")
+
+
+
+
+
+//number,boolean,string,object
+   
+
+const button  = document.querySelector('button')
+button.onclick = () => {
+    console.log(`txtUsername: ${txtUsername.value},txtPassword: ${txtPassword}
+    ,cnfrmPassword: ${cnfrmPassword},txtage: ${txtAge}`)
+}
+
+
+    //alert("Register button has been clicked.")
